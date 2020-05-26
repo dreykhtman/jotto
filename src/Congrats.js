67041eda02184fed7f71d030521c2ca6027c96e1
Dima <dreykhtman@gmail.com>
2020-05-26 15:12:43 -0400

    added Congrats component and tests

--- src/Congrats.js
+++ src/Congrats.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
-const Congrats = () => {
-  return <div />;
+const Congrats = ({ success }) => {
+  if (success) {
+    return (
+      <div data-test="component-congrats">
+        <span data-test="congrats-message">
+          Congratulations! You guessed the word!
+        </span>
+      </div>
+    );
+  }
+  return <div data-test="component-congrats" />;
 };
 
 export default Congrats;
