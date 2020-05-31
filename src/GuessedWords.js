93be2991901006e1f85a8c88e7646e120474786b
Dima <dreykhtman@gmail.com>
2020-05-30 21:20:18 -0400

    created GuessedWords component and test

---
+++ src/GuessedWords.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const GuessedWords = () => {
+  return <div>a</div>;
+};
+
+GuessedWords.propTypes = {};
+
+export default GuessedWords;
