5d282c2f3776a75e1e8b33adf1a9e8281493775d
Dima <dreykhtman@gmail.com>
2020-06-02 15:45:33 -0400

    4.45-4.46 created tests and code for non-zero guessed words

--- src/GuessedWords.js
+++ src/GuessedWords.js
@@ -7,6 +7,27 @@ const GuessedWords = ({ guessedWords }) => {
     contents = (
       <span data-test="guess-instructions">Try to guess the secret word!</span>
     );
+  } else {
+    const guessedWordsRows = guessedWords.map((word, i) => (
+      <tr data-test="guessed-word" key={i}>
+        <td>{word.guessedWord}</td>
+        <td>{word.letterMatchCount}</td>
+      </tr>
+    ));
+    contents = (
+      <div data-test="guessed-words">
+        <h3>Guessed Words</h3>
+        <table>
+          <thead>
+            <tr>
+              <th>Guess</th>
+              <th>Matching Letters</th>
+            </tr>
+          </thead>
+          <tbody>{guessedWordsRows}</tbody>
+        </table>
+      </div>
+    );
   }
 
   return <div data-test="component-guessed-words">{contents}</div>;
